65e074d1e0829ac3154c708e78d7da5d31f91e64
HerbertAMS <herbert.ams@gmail.com>
2023-01-27 13:11:32 -0300

    dev sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
-    <h1>Hello Word!</h1>
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Herbert" content="Hello, World!" />
+          <Post author="Geise" content="Hey dad!" />
+        </main>
+      </div>
+    </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+      className={styles.cover}      
+      src="https://images.unsplash.com/photo-1646666477664-d19273808d5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZW5nZW5lZXJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=50" />
+
+      <div className={styles.profile}>
+        <img 
+            className={styles.avatar}
+            src="https://github.com/HerbertAMS.png" 
+        
+        />
+        <strong>Herbert Severino</strong>
+        <span>Mechanical Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}

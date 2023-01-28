307870565209e37d6ad1a2b3fd4a17dac3ff0664
HerbertAMS <herbert.ams@gmail.com>
2023-01-28 17:35:55 -0300

    dev Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 import styles from "./App.module.css";
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -11,8 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Herbert" content="Hello, World!" />
-          <Post author="Geise" content="Hey dad!" />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import styles from "./Post.module.css";
+
+export function Post(props) {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/HerbertAMS.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Herbert Severino</strong>
+            <span>Mechanical Engineer</span>
+          </div>
+        </div>
+
+        <time title="27 Janeiro às 18:17h" dateTime="2023-01-27 18:17:04">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#"> #novoprojeto </a>
+          <a href="#"> #nlw </a>
+          <a href="#"> #rocketseat </a>
+        </p>
+      </div>
+    </article>
+  );
+}

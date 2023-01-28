b995c47cfa3b9edfeae8044a1123c5a204c36659
HerbertAMS <herbert.ams@gmail.com>
2023-01-28 19:15:24 -0300

    dev Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https:/github.com/HerbertAMS.png"/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Herbert Severino</strong>
+                            <span>Mechanical Engineer</span>
+                            <time title="27 Janeiro às 18:17h" dateTime="2023-01-27 18:17:04">
+                                Cercar de 1h atŕas
+                            </time>
+
+                        </div>
+
+                        <button title="Deletar um comentário">
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+
+        </div>
+    )
+}

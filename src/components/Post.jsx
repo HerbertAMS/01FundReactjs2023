ee9040aaded7528db5a158879aa5486b6973d329
HerbertAMS <herbert.ams@gmail.com>
2023-01-30 12:02:46 -0300

    aplly useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,32 @@
-import {format, formatDistanceToNow} from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
 
 import styles from "./Post.module.css";
+import { useState } from "react";
 
-export function Post({author, publishedAt, content}) {
-const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
-  locale: ptBR,  
-})
-  // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
-  //   day: '2-digit',
-  //   month: 'long',
-  //   hour: '2-digit',
-  //   minute: '2-digit',
-  // }).format(publishedAt);
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2]);
 
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
   const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true,
-  })
+  });
+
+  function handleCreateComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+    console.log("Creating comment");
+  }
 
   return (
     <article className={styles.post}>
@@ -33,37 +39,41 @@ const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
+        {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>
+            return <p>{line.content}</p>;
           } else if (line.type === "link") {
-            return <p><a href="#">{line.content}</a></p>
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
-
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea 
-          placeholder="Deixe seu comentário"
-        />
+        <textarea placeholder="Deixe seu comentário" />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
